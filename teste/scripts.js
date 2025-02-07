0bf1e2a8f7171b2a297b1b84aa82a34b19213a66
julianaMonteiro <juliana47monteiro@gmail.com>
2025-02-07 12:07:07 -0300

    estilização

---
+++ teste/scripts.js
@@ -0,0 +1,12 @@
+let turma = "Turma 79";
+
+function helloWorld() {
+    console.log("Hello World " + turma)
+}
+
+let nome = document.querySelector("#nome");
+
+
+function enviar() {
+    alert("Olá " + nome.value);
+}
